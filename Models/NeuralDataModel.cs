a80ed40485e56935186e2cca0b3fe9fd65681d2e
PhamVuThuNguyet <pvtnguyet.19it1@vku.udn.vn>
2023-01-09 17:58:59 +0700

    Init project

---
+++ Models/NeuralDataModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralLab.Models
+{
+    sealed public class NeuralDataModel: LinkedList<NeuralDataModel>
+    {
+        public readonly int Id;
+        public long VisualId;
+
+        public double X; // Value used as a param of activation function;   
+    }
+}
